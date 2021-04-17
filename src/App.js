2bb92e8cde336ff27a84cd6bccc2c17960e411cd
SAKSHAM KUMAR <64663891+kumarsaksham@users.noreply.github.com>
2021-04-17 18:46:37 +0530

    App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Header';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  return (
+      <div className="App">
+        <Header />
+        <About />
+        <Contact />
+      </div>
+  );
+}
+
+export default App;
