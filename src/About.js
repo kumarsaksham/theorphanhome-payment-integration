7adc686708990137045f50871052be1704a21bf2
SAKSHAM KUMAR <64663891+kumarsaksham@users.noreply.github.com>
2021-04-17 18:47:56 +0530

    about us component

---
+++ src/About.js
@@ -0,0 +1,20 @@
+import './About.css';
+
+function About(){
+    return(
+        <div className="about" id='about'>
+            <h1>About Us</h1>
+
+            <p>The Orphan Home is a registered Non Profitable Charitable Organization under the Society's registration Act of 1960 (No.184/86-87). Constituted in 1980. We offer service to the Young at Risk like - children living on the streets, child labourers, abandoned / orphaned children,begging children, rag pickers etc.</p>
+
+            <h2>Vision</h2>
+            <p>To build a community that will unreservedly care for its children through a preventive and promotional approach which supports children and activates forces in the society that favour them.</p>
+
+            <h2>Mission</h2>
+            <p>To encourage the young at risk to join the mainstream of society as educated and productive citizens by partnering with the children in their daily struggle to grow off the streets.</p>
+
+        </div>
+    );
+}
+
+export default About;
